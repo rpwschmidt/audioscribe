943da016ee26422fdc5f043ca94328e00b08bfc3
ralfschmidt <107412896+rpwschmidt@users.noreply.github.com>
2024-08-29 14:43:04 +0200

    Update audioscribe.py

--- audioscribe.py
+++ audioscribe.py
@@ -1,80 +1,102 @@
 import os
 import torch
-import whisper
+from faster_whisper import WhisperModel
 import gradio as gr
 from datetime import datetime
 from moviepy.editor import VideoFileClip
 
 
 def convert_video(folder):
+    """
+    Load in a video and extract the audio from it. Then, save the audio as mp3 and transcribe it.
+    """
     start = datetime.now()
-
     for file in folder:
         # Save the temporary filename, extract the audio and save it
-        original_name = file.name.split('\\')[-1]
-        file_name = file.name
-        clip = VideoFileClip(file_name)
+        _, original_name = os.path.split(file.name)
+        filename, _ = os.path.splitext(original_name)
+
+        # original_name = file.name.split('/')[-1]
+        # file_name = file.name
+        clip = VideoFileClip(file.name)
 
         # Define the name of the output file
-        new_file = f"./audiodata/{original_name.split('.')[0]}.mp3"
+        new_file = f"./audiodata/{filename}.mp3"
         clip.audio.write_audiofile(new_file, logger=None)
-        transcribe_save(new_file, original_name)
-    return f'Done! {len(folder)} video files processed and created.\nSaved in ./audioscribe/output/ folder!\nTook {datetime.now()-start}'
+        # Remove the temporary video file after extracting the audio to clear memory
+        del clip
+        transcribe_save(new_file, filename)
+
+    return (f'Done! {len(folder)} video files processed and created.\n'
+            f'Saved in ./audioscribe/output/ folder!\n'
+            f'Took {datetime.now()-start}')
 
 
 def convert_audio(folder):
+    """
+    Load in an audio file and transcribe it.
+    """
     start = datetime.now()
     for file in folder:
-        transcribe_save(file.name, file.name.split('\\')[-1])
-    return f'Done! {len(folder)} audio files processed and created.\nSaved in ./audioscribe/output/ folder!\nTook {datetime.now()-start}'
+        # Get the filename without extension
+        _, original_name = os.path.split(file.name)
+        filename, _ = os.path.splitext(original_name)
+
+        # Transcribe the audio file and save the output to a text file
+        transcribe_save(file.name, filename)
+    return (f'Done! {len(folder)} audio files processed and created.\n'
+            f'Saved in ./audioscribe/output/ folder!\n'
+            f'Took {datetime.now()-start}')
 
 
-def transcribe_save(fileloc, filename):
-    transcript = model.transcribe(fileloc, fp16=False)
-    with open(f'./output/{filename.split(".")[0]}.txt', 'w', encoding="utf-8") as output:
-        for segment in transcript['segments']:
-            text = segment['text'].replace('. ', '.\n').replace('? ', '?\n').replace('! ', '!\n')
-            output.write(f"[{round(segment['start'], 2)}s - {round(segment['end'], 2)}s]:\n{text[1:]}\n\n")
+def transcribe_save(file_loc, filename):
+    """
+    Transcribe the audio file and save the output to a text file.
+    """
+    transcript, info = model.transcribe(file_loc)
+    with open(f"./output/{filename}.txt", "w", encoding="utf-8") as output:
+        for sentence in transcript:
+            timestamp = f"[{round(sentence.start, 2)}s - {round(sentence.end, 2)}s]\n"
+            text = sentence.text.replace('. ', '.\n').replace('? ', '?\n').replace('! ', '!\n').lstrip()
+            output.write(timestamp + text + '\n\n')
 
 
 def initialize(model_size, use_gpu):
     """
-    Function to choose and load a preferred model. Any gpu model selection is available, but the script will fall back
+    Function to choose and load a preferred model. Any GPU model selection is available, but the script will fall back
     to the next best option if the hardware does not support it.
-    Cpu models are unrestricted and will load regardless of gpu availability.
+    CPU models are restricted on RAM and will load regardless of GPU availability.
     """
     global model
     MEMORY = torch.cuda.mem_get_info()[-1] if torch.cuda.is_available() else 0
-    mem_size = {'small': 2e9, 'medium': 5e9, 'large-v3': 10e9}
-
     try:
         if use_gpu:
             # Try the chosen option first
-            if torch.cuda.is_available() and (MEMORY >= mem_size[model_size]):
-                model = whisper.load_model(model_size, 'cuda')
-                return f"Using the {model_size} model, running on gpu"
-
-            # Go down the ladder to find the next best option
-            elif torch.cuda.is_available() and (MEMORY >= mem_size['medium']):
-                model = whisper.load_model('medium', 'cuda')
-                return f"Using the 'medium' model, running on gpu"
-
-            elif torch.cuda.is_available() and (MEMORY >= mem_size['small']):
-                model = whisper.load_model('small', 'cuda')
-                return f"Using the 'small' model, running on gpu"
-
+            if torch.cuda.is_available() and (MEMORY >= 3.8e9):
+                model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
+                return f"Using the {model_size.upper()} model, running on GPU"
+            elif torch.cuda.is_available() and (MEMORY >= 2.8e9):
+                model = WhisperModel('medium', device="cuda", compute_type="int8_float32")
+                return f"Using the MEDIUM model, running on GPU"
+            elif torch.cuda.is_available() and (MEMORY >= 1.8e9):
+                model = WhisperModel('small', device="cuda", compute_type="int8_float32")
+                return f"Using the SMALL model, running on GPU"
             else:
-                model = whisper.load_model(model_size, 'cpu')
-                return f"No gpu available, using the {model_size} model, running on cpu"
+                model = WhisperModel(model_size, device="cpu", compute_type="auto")
+                return f"No GPU available, using the {model_size.upper()} model, running on CPU"
         else:
-            model = whisper.load_model(model_size, 'cpu')
-            return f"Using the {model_size} model, running on cpu"
+            model = WhisperModel(model_size, device="cpu", compute_type="auto")
+            return f"Using the {model_size.upper()} model, running on CPU"
 
     except RuntimeError:
         print("An error has been encountered.")
 
 
 def interface():
+    """
+    Create the Gradio interface for the user to interact with. The user can select the model size and whether to use the GPU.
+    They can then upload audio or video files to transcribe. The output will be saved in the output folder.
+    """
     # Select a model to use for transcription
     m_size = gr.Dropdown(label="Select model size", choices=["small", "medium", "large-v3"], value="large-v3", interactive=True)
     check = gr.Checkbox(label="Use GPU (if available)", value=True, interactive=True)
